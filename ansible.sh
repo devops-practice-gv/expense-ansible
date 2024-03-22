15abb74d08750c250e46cba0200bf28bef219ed7
Gaurav <gauravenigma@gmail.com>
2024-03-22 05:04:18 +0000

    provisioner Terraform

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
-ansible-playbook -i $component-dev.makemyhomesmart.co.uk. -e ansible-user=ec2user -e ansible-password=DevOps321 -e env=dev -e role_name=$component expense.yml
+ansible-playbook -i $component-dev.makemyhomesmart.co.uk. -e ansible_user=ec2user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
 
