71d61a0ab819a4ec1ca1dfd5aa1b21b04ec7b9ec
Gaurav <gauravenigma@gmail.com>
2024-03-24 00:34:20 +0000

    Expense app Terraform

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,3 @@
 component=$1
 ansible-playbook -i $component-dev.makemyhomesmart.co.uk, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
 
-component=$1
